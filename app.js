728f1135fdd354562541e3e02434fcb20f7d082c
SahilRathore26 <saheelathore@gmail.com>
2025-07-08 19:09:00 +0530

    episode-1

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-/* 
-<div class="parent">
-  <div class="child">
-    <h1>Heading h1 tag</h1>
-    <h2>Heading h2 tag</h2>
-  </div>
-</div> 
-*/
-
-const parent = React.createElement("div", {id:"parent"}, 
-  React.createElement("div", {id:"child"}, [
-   React.createElement("h1", {}, "Heading h1 tag"), 
-   React.createElement("h2", {}, "Heading h2 tag"),
-  ]));
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Heading h1 tag"),
+    React.createElement("h2", {}, "Heading h2 tag"),
+  ])
+);
 console.log(parent);
 
 // const heading = React.createElement("h1", {}, "Hello World");
